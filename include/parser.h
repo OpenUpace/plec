b7d5c02b1425ffaf9baa35d6e7546ecacc496478
Ai-Upace <jntmngmhahayo@outlook.com>
2025-09-08 19:18:24 +0800

    feat(parser): port the old parser.

---
+++ include/parser.h
@@ -0,0 +1,25 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <lexer.h>
+#include <ast.h>
+
+class Parser {
+public:
+    explicit Parser(Lexer lexer);
+
+private:
+    Token Advance();
+    Token Advance();         // 消费当前 Token，获取下一个
+    [[nodiscard]] Token Peek() const;            // 查看当前 Token
+    bool Match(TokenType expectedType);   // 如果当前 Token 是目标类型，就前进
+    [[nodiscard]] bool IsAtEnd() const;
+    [[nodiscard]] bool Check(TokenType expectedType) const;
+
+    ASTNode* paserExpression();
+    ASTNode* parsePrimary();
+    ASTNode* parseAdditiveExpression();
+    ASTNode* parseMultiplicative();
+};
+
+#endif
