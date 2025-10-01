015fb48783e6fd1def09e631340ed3289b8f08f6
Ai-Upace <jntmngmhahayo@outlook.com>
2025-10-01 22:07:17 +0800

    feat(parser): add the simple define.
    This is a sample parser, add the define function.

--- include/parser.h
+++ include/parser.h
@@ -9,17 +9,19 @@ public:
     explicit Parser(Lexer lexer);
 
 private:
-    Token Advance();
-    Token Advance();         // 消费当前 Token，获取下一个
-    [[nodiscard]] Token Peek() const;            // 查看当前 Token
-    bool Match(TokenType expectedType);   // 如果当前 Token 是目标类型，就前进
+    Token Advance();                                // 消费当前 Token，获取下一个
+    [[nodiscard]] Token Peek() const;               // 查看当前 Token
+    bool Match(TokenType expectedType);             // 如果当前 Token 是目标类型，就前进
     [[nodiscard]] bool IsAtEnd() const;
     [[nodiscard]] bool Check(TokenType expectedType) const;
 
     ASTNode* paserExpression();
     ASTNode* parsePrimary();
-    ASTNode* parseAdditiveExpression();
-    ASTNode* parseMultiplicative();
+    ASTNode* parseAdditiveExpr();
+    ASTNode* parseMultiplicativeExpr();
+
+    Lexer lexer;
+    Token currentToken;
 };
 
 #endif
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,1 +1,3 @@
 #include "../include/parser.h"
+
+Parser::Parser(Lexer lexer) : lexer(std::move(lexer)), currentToken(this->lexer.NextToken()) {}
