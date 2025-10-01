33eb9e0ff21e4193736e55dd2e8283b34d3e7e6c
Ai-Upace <jntmngmhahayo@outlook.com>
2025-10-01 22:28:38 +0800

    feat(parser): add the simple function.

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,3 +1,20 @@
 #include "../include/parser.h"
+#include "../include/ast.h"
 
-Parser::Parser(Lexer lexer) : lexer(std::move(lexer)), currentToken(this->lexer.NextToken()) {}
+Parser::Parser(Lexer lexer) : 
+    lexer(std::move(lexer)), currentToken(this->lexer.NextToken()) {}
+
+Token Parser::Advance()
+{
+    currentToken = lexer.NextToken();
+    return currentToken;
+}
+
+Token Parser::Peek() const
+{
+    return currentToken;
+}
+
+bool Parser::IsAtEnd() const {
+    return currentToken.type == TokenType::EOF_TOKEN;
+}
