9f438fcbf98faaff02df6cf9c503455df946f717
Ai-Upace <jntmngmhahayo@outlook.com>
2026-02-08 15:36:28 +0800

    feat: Create the Parser.h/cpp file and remake the old parser. But it has the hack.

---
+++ include/catm/Parser/Parser.h
@@ -0,0 +1,45 @@
+#ifndef CATMC_PARSER_H
+#define CATMC_PARSER_H
+
+#include "catm/Lex/Lexer.h"
+#include "catm/Parser/ASTNode.h"
+#include <map>
+
+namespace catm {
+class Parser {
+  public:
+    explicit Parser(Lexer lexer) : lexer(lexer) {}
+    void Parse();
+
+  private:
+    Lexer lexer;
+    Token CurTok;
+    std::map<char, int> BinopPrecedence;
+
+    Token getNextToken();
+    std::unique_ptr<ExprAST> LogError(const char *Str);
+    std::unique_ptr<catm::PrototypeAST> LogErrorP(const char *Str);
+    int GetTokPrecedence();
+
+    std::unique_ptr<ExprAST> ParseExpression();
+    std::unique_ptr<ExprAST> ParseNumberExpr();
+    std::unique_ptr<ExprAST> ParseParenExpr();
+    std::unique_ptr<ExprAST> ParseIdentifierExpr();
+    std::unique_ptr<ExprAST> ParseIfExpr();
+    std::unique_ptr<ExprAST> ParseForExpr();
+    std::unique_ptr<ExprAST> ParsePrimary();
+    std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec,
+                                           std::unique_ptr<ExprAST> LHS);
+    std::unique_ptr<PrototypeAST> ParsePrototype();
+    std::unique_ptr<FunctionAST> ParseDefinition();
+    std::unique_ptr<FunctionAST> ParseTopLevelExpr();
+    std::unique_ptr<PrototypeAST> ParseExtern();
+
+    void HandleDefinition();
+    void HandleExtern();
+    void HandleTopLevelExpression();
+    void MainLoop();
+};
+} // namespace catm
+
+#endif // !CATMC_PARSER_H
