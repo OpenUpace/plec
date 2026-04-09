412620cf97ab7bf2b4e49859d9eb68b1a8d8552c
Ai-Upace <jntmngmhahayo@outlook.com>
2026-04-09 21:58:49 +0800

    feat(debug): come back the origin main function.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,37 @@
-use plec::prelude::repl::*;
+use chumsky::Parser;
+use logos::Logos;
+use plec::prelude::lexer::*;
+use plec::prelude::parser::*;
+// use plec::prelude::repl::*;
 
 fn main() {
-    let _ = repl();
+    let input = std::env::args()
+        .nth(1)
+        .expect("Expected expression argument (e.g. `1 + 7 * (3 - 4) / 5`)");
+    let lexer = Token::lexer(&input);
+
+    //splits the input into tokens, using the lexer
+    let mut tokens = vec![];
+    for (token, span) in lexer.spanned() {
+        match token {
+            Ok(token) => tokens.push(token),
+            Err(e) => {
+                println!("lexer error at {:?}: {}", span, e);
+                return;
+            }
+        }
+    }
+
+    //parses the tokens to construct an AST
+    let _ast = match parser().parse(&tokens).into_result() {
+        Ok(expr) => {
+            println!("[AST]\n{:#?}", expr);
+            expr
+        }
+        Err(e) => {
+            println!("parse error: {:#?}", e);
+            return;
+        }
+    };
+    // let _ = repl(); // debug.
 }
