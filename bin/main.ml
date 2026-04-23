d9cf18b25fe0abf5dcb974ac9b64ca25e2995961
Ai-Upace <jntmngmhahayo@outlook.com>
2026-04-23 18:45:35 +0800

    fix(test): fix the hack and add the debug function.
    Okay, I have implemented the debug function(see source code, e.g.
    string_of_ty). But the main.ml has some debug function. So plec has hack
    yet.
    
    TODO: fix the hack, then use sedlex and menhir to implement the fontend.

--- bin/main.ml
+++ bin/main.ml
@@ -3,15 +3,42 @@ open Types
 open Semantics
 open Eval
 
+(* For Debug *)
+let rec string_of_ty = function
+  | Bool -> "Bool"
+  | Arrow (t1, t2) -> "(" ^ string_of_ty t1 ^ " -> " ^ string_of_ty t2 ^ ")"
+
+let rec string_of_term = function
+  | Var x -> x
+  | Lam (fn, ty, arg) ->
+      "<fun> " ^ fn ^ string_of_ty ty  ^ string_of_term arg
+  | App (fn, arg) -> "<abs> " ^ string_of_term fn ^ string_of_term arg
+  | BoolLit b -> string_of_bool b
+  | If (cond, when_true, when_false) ->
+      "<if> " ^ string_of_term cond ^ "<then> " ^ string_of_term when_true
+      ^ "<else> " ^ string_of_term when_false
+
+let rec string_of_type_error = function
+  | Unbound_variable str -> "unbound variable: " ^ str
+  | Not_a_function ty -> "trying to apply a non-function value of type " ^ string_of_ty ty
+  | Mismatch { expected; found } -> "expected " ^ string_of_ty expected ^ " found " ^ string_of_ty found
+
+let test term =
+  try
+    let v = eval term in
+    print_endline ("Result " ^ string_of_term v);
+
+    match type_of StringMap.empty term with
+    | Ok ty -> print_endline ("Type: " ^ string_of_ty ty);
+    | Error err -> print_endline ("Type error: " ^ string_of_type_error err);
+  with e -> print_endline ("Error: " ^ Printexc.to_string e)
+
+(* main *)
 (* Small sample *)
 let identity_bool = Lam ("x", Bool, Var "x")
 
 let sample_program =
   If (BoolLit true, App (identity_bool, BoolLit true), BoolLit false)
 
-let sample_type = type_of StringMap.empty sample_program
-
-(* Hack: cannot print the eval result *)
 let () =
-  let _ = eval sample_program in
-  print_endline "End."
+  test sample_program
