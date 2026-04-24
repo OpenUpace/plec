4d43e3ecf8fda93f4f1475e88e2f933c14b1a680
Ai-Upace <jntmngmhahayo@outlook.com>
2026-04-24 13:35:13 +0800

    feat(debug): remove the test eval to 'debug.ml'.

--- bin/main.ml
+++ bin/main.ml
@@ -1,19 +1,8 @@
 open Plec
 open Types
-open Semantics
 open Eval
 open Error
 open Debug
-
-let test term =
-  try
-    let v = eval term in
-    print_endline ("Result " ^ string_of_term v);
-
-    match type_of StringMap.empty term with
-    | Ok ty -> print_endline ("Type: " ^ string_of_ty ty)
-    | Error err -> print_endline ("Type error: " ^ string_of_type_error err)
-  with e -> print_endline ("Error: " ^ Printexc.to_string e)
 
 (* main *)
 (* Small sample *)
--- lib/diagnostics/debug.ml
+++ lib/diagnostics/debug.ml
@@ -1,5 +1,7 @@
 open Types
 open Error
+open Semantics
+open Eval
 
 let rec string_of_term = function
   | Var x -> x
@@ -11,3 +13,13 @@ let rec string_of_term = function
   | If (cond, when_true, when_false) ->
       " <if> " ^ string_of_term cond ^ " <then> " ^ string_of_term when_true
       ^ " <else> " ^ string_of_term when_false
+
+let test term =
+  try
+    let v = eval term in
+    print_endline ("Result " ^ string_of_term v);
+
+    match type_of StringMap.empty term with
+    | Ok ty -> print_endline ("Type: " ^ string_of_ty ty)
+    | Error err -> print_endline ("Type error: " ^ string_of_type_error err)
+  with e -> print_endline ("Error: " ^ Printexc.to_string e)
